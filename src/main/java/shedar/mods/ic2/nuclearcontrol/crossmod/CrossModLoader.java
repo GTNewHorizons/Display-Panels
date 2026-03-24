c1b404ca84cd90046622bcacdde9f5ce8daad1b0
boubou19 <miisterunknown@gmail.com>
2026-03-24 13:20:40 +0100

    reference waila class through class object instead of hardcoded string

--- src/main/java/shedar/mods/ic2/nuclearcontrol/crossmod/CrossModLoader.java
+++ src/main/java/shedar/mods/ic2/nuclearcontrol/crossmod/CrossModLoader.java
@@ -4,6 +4,7 @@ import cpw.mods.fml.common.event.FMLInterModComms;
 import shedar.mods.ic2.nuclearcontrol.crossmod.RF.CrossTE;
 import shedar.mods.ic2.nuclearcontrol.crossmod.appeng.CrossAppeng;
 import shedar.mods.ic2.nuclearcontrol.crossmod.vanilla.Vanilla;
+import shedar.mods.ic2.nuclearcontrol.crossmod.waila.CrossWaila;
 
 public class CrossModLoader {
 
@@ -12,7 +13,7 @@ public class CrossModLoader {
         FMLInterModComms.sendMessage(
                 "Waila",
                 "register",
-                "shedar.mods.ic2.nuclearcontrol.crossmod.waila.CrossWaila.callbackRegister");
+                CrossWaila.class.getName()+".callbackRegister");
         CrossAppeng.registrationCheck();
         Vanilla.initVanilla();
     }
