18aca14e0db7d93ca0f1e6aa2b9f12da4cfbec52
boubou19 <miisterunknown@gmail.com>
2026-03-24 21:23:54 +0100

    fix missing imports

--- src/main/java/shedar/mods/ic2/nuclearcontrol/crossmod/waila/AverageCounterProvider.java
+++ src/main/java/shedar/mods/ic2/nuclearcontrol/crossmod/waila/AverageCounterProvider.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 
 import mcp.mobius.waila.api.IWailaConfigHandler;
